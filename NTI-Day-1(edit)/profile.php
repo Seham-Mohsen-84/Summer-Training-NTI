cea38e3023ff9d3138643b1fb773ab76f6662fc4
SehamMohsen <sehammohsen@std.mans.edu.eg>
2025-07-14 23:29:07 +0300

    Add files via upload

---
+++ NTI-Day-1(edit)/profile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Profile</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-success-subtle vh-100 d-flex justify-content-center align-items-center">
+
+<div class="card p-4 shadow" style="width: 300px;">
+    <h5 class="text-center mb-4">User Profile</h5>
+    <a href="form.php" class="btn btn-primary w-100">Back to Form</a>
+</div>
+</body>
+</html>
