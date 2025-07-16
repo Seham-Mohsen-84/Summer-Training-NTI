fb5bca2165318b75f1de80fa51667ee9454dc9ab
SehamMohsen <sehammohsen@std.mans.edu.eg>
2025-07-16 18:35:02 +0300

    Add files via upload

--- NTI-Day-4/Task2.php
+++ NTI-Day-4/Task2.php
@@ -1,17 +1,17 @@
 <?php
 $array=[
     [
-     "name"=>"Laptop",
-    "price"=>3000,
-    "quantity"=>"1"
+        "name"=>"Laptop",
+        "price"=>3000,
+        "quantity"=>"1"
     ],
     [
-        "name"=>"Pc",
+        "name"=>"Personal computer",
         "price"=>9000,
         "quantity"=>"3"
     ],
     [
-       "name"=>"phone",
+        "name"=>"phone",
         "price"=>99900,
         "quantity"=>"4"
     ]
@@ -23,7 +23,7 @@ $array=[
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="../NTI-Day-1/css/bootstrap.css" rel="stylesheet">
-    <title>Task1</title>
+    <title>Task2</title>
 </head>
 <body class="bg-dark-subtle text-black">
 <div class="container">
@@ -38,13 +38,15 @@ $array=[
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($array as $value):?>
-                <tr>
-                    <td><?php echo $value["name"]?></td>
-                    <td><?php echo $value["price"]?></td>
-                    <td><?php echo $value["quantity"]?></td>
-                </tr>
-                <?php endforeach; ?>
+                <?php foreach ($array as $value):
+                    if ($value['price']>=7000) :?>
+                    <tr>
+                        <td><?php echo $value["name"]?></td>
+                        <td><?php echo $value["price"]?></td>
+                        <td><?php echo $value["quantity"]?></td>
+                    </tr>
+
+                <?php endif; endforeach; ?>
                 </tbody>
             </table>
         </div>
