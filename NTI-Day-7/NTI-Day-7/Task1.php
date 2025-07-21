c9af5d11f21c8e30dc0250cebedffbed7e040355
SehamMohsen <sehammohsen84@gmail.com>
2025-07-21 22:00:18 +0300

    Delete NTI-Day-7/NTI-Day-7 directory

--- NTI-Day-7/NTI-Day-7/Task1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();
-$_SESSION['visits'] = ($_SESSION['visits'] ?? 0) + 1;
-?>
-<p class="m-3">Page Visits: <span class="badge bg-primary"><?= $_SESSION['visits'] ?></span></p>
